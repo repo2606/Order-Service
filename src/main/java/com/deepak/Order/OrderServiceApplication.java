86f7b91090bf6e8b888a9fbe9f643c46c65b3e42
David <learn-git-fast@abc.com>
2025-04-03 00:52:57 -0400

    final commit

--- src/main/java/com/deepak/Order/OrderServiceApplication.java
+++ src/main/java/com/deepak/Order/OrderServiceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OrderServiceApplication.class, args);
+
+		System.out.println("mild changes");
 	}
 
 }
